b340048032cb6d5cc22adcb18b9af0d5134b63e9
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-04-07 15:03:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-from sentence_transformers import SentenceTransformer, util
+from transformers import AutoTokenizer, AutoModel
+import torch
 
 class InferlessPythonModel:
     def initialize(self):
-        self.model = SentenceTransformer("jinaai/jina-embeddings-v2-base-en",trust_remote_code=True)
-        # control your input sequence length up to 8192
-        self.model.max_seq_length = 1024
+        self.tokenizer = AutoTokenizer.from_pretrained('intfloat/multilingual-e5-large')
+        self.model = AutoModel.from_pretrained('intfloat/multilingual-e5-large').to("cuda")
+
+    def mean_pooling(self, model_output, attention_mask):
+        token_embeddings = model_output[0]
+        input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+        return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
 
     def infer(self, inputs):
-        sentences = inputs["sentences"]
-        embeddings = self.model.encode(sentences)
-        return {"result": embeddings}
+        sentence = inputs["sentence"]
+        encoded_input = self.tokenizer(sentence, padding=True, truncation=True, return_tensors='pt').to("cuda")
+        with torch.no_grad():
+            model_output = self.model(**encoded_input)
+        sentence_embeddings = self.mean_pooling(model_output, encoded_input['attention_mask'])
+        return {"embedding":sentence_embeddings.tolist()[0]}
+
     def finalize(self, args):
-        self.pipe = None
+        self.model = None
+        self.tokenizer = None
